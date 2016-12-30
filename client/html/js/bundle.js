8299f979f95c338a7f89b66741b5c73467cdd9c8
Sean Dawson <contact@seandawson.info>
2016-12-30 16:54:42 +1100

    Adjust colours

--- client/html/js/bundle.js
+++ client/html/js/bundle.js
@@ -1,7 +1,9 @@
 (function (L) {
 'use strict';
 
-var layer = new L.StamenTileLayer("toner-background");
+var layer = new L.StamenTileLayer("toner-background", {
+  opacity: 0.5
+});
 var map = new L.Map("mapid", {
   center: new L.LatLng(-37.8136, 144.9631),
   zoom: 12
@@ -17,7 +19,7 @@ Promise.all([fetch('geojson/bus-lines-simplified.json').then(function (res) {
 })]).then(function (results) {
   L.geoJSON(results[0], {
     style: {
-      "color": "#FF6F00",
+      "color": "#FFB74D",
       "weight": 2,
       "opacity": 0.25
     }
--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,9 @@
 import * as L from 'Leaflet';
 
 
-var layer = new L.StamenTileLayer("toner-background");
+var layer = new L.StamenTileLayer("toner-background", {
+  opacity: 0.5,
+});
 var map = new L.Map("mapid", {
     center: new L.LatLng(-37.8136, 144.9631),
     zoom: 12,
@@ -17,7 +19,7 @@ Promise.all([
   .then((results) => {
     L.geoJSON(results[0], {
         style: {
-          "color": "#FF6F00",
+          "color": "#FFB74D",
           "weight": 2,
           "opacity": 0.25,
         }

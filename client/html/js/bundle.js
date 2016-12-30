05276fc1d325b86b1c85c368ff401a48a363636b
Sean Dawson <contact@seandawson.info>
2016-12-30 12:58:50 +1100

    Add transport lines to the map

--- client/html/js/bundle.js
+++ client/html/js/bundle.js
@@ -1,12 +1,52 @@
 (function (L) {
 'use strict';
 
-var layer = new L.StamenTileLayer("toner");
+var layer = new L.StamenTileLayer("toner-background");
 var map = new L.Map("mapid", {
-    center: new L.LatLng(-37.8136, 144.9631),
-    zoom: 12
+  center: new L.LatLng(-37.8136, 144.9631),
+  zoom: 12
 });
-
 map.addLayer(layer);
+
+Promise.all([fetch('geojson/bus-lines-simplified.json').then(function (res) {
+  return res.json();
+}), fetch('geojson/tram-lines-simplified.json').then(function (res) {
+  return res.json();
+}), fetch('geojson/train-lines-simplified.json').then(function (res) {
+  return res.json();
+})]).then(function (results) {
+  L.geoJSON(results[0], {
+    style: {
+      "color": "#FF6F00",
+      "weight": 2,
+      "opacity": 0.25
+    }
+  }).addTo(map);
+
+  L.geoJSON(results[1], {
+    style: {
+      "color": "#7CB342",
+      "weight": 3,
+      "opacity": 0.75
+    }
+  }).addTo(map);
+
+  L.geoJSON(results[2], {
+    style: {
+      "color": "#01579B",
+      "weight": 4,
+      "opacity": 1
+    }
+  }).addTo(map);
+
+  map.createPane('labels');
+  map.getPane('labels').style.zIndex = 650;
+  map.getPane('labels').style.pointerEvents = 'none';
+
+  var layer = new L.StamenTileLayer("toner-labels", {
+    pane: 'labels'
+  });
+  map.addLayer(layer);
+});
 
 }(L));

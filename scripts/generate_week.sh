34d74b39be8595f8ae71735a072f408a14e1e447
Sean Dawson <contact@seandawson.info>
2017-01-08 11:53:25 +1100

    Add script to dump json for an entire week

---
+++ scripts/generate_week.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+./generate_time_series.py -d data/trains -o 20170109.json -D 2017-01-09 # Monday
+./generate_time_series.py -d data/trains -o 20170110.json -D 2017-01-10 # Tueday
+./generate_time_series.py -d data/trains -o 20170111.json -D 2017-01-11 # Wednesday
+./generate_time_series.py -d data/trains -o 20170112.json -D 2017-01-12 # Thursday
+./generate_time_series.py -d data/trains -o 20170113.json -D 2017-01-13 # Friday
+./generate_time_series.py -d data/trains -o 20170114.json -D 2017-01-14 # Saturday
+./generate_time_series.py -d data/trains -o 20170115.json -D 2017-01-15 # Sunday
